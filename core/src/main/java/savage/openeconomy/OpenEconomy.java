cdaeb41cada30d5770cfa79eed7bfed37d6afdda
xSaVageAU <jessejd1997@gmail.com>
2026-05-03 21:18:17 +0930

    move pay command from core to implementation

--- core/src/main/java/savage/openeconomy/OpenEconomy.java
+++ core/src/main/java/savage/openeconomy/OpenEconomy.java
@@ -2,13 +2,11 @@ package savage.openeconomy;
 
 import eu.pb4.common.economy.api.CommonEconomy;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import savage.openeconomy.command.EconomyCommands;
 import savage.openeconomy.core.EconomyManager;
 import savage.openeconomy.integration.OpenEconomyProvider;
 import savage.openeconomy.messaging.MessagingRegistry;
@@ -30,9 +28,6 @@ public class OpenEconomy implements ModInitializer {
 
         StorageRegistry.discoverProviders();
         MessagingRegistry.discoverProviders();
-
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> 
-            EconomyCommands.register(dispatcher));
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, srv) -> 
             EconomyManager.getInstance().getOrCreateAccount(handler.getPlayer().getUUID(), handler.getPlayer().getGameProfile().name()));
--- mod/src/main/java/savage/openeconomy/mod/command/ModEconomyCommands.java
+++ mod/src/main/java/savage/openeconomy/mod/command/ModEconomyCommands.java
@@ -8,6 +8,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import savage.openeconomy.core.EconomyManager;
 import savage.openeconomy.util.CurrencyFormatter;
+import java.math.BigDecimal;
 
 /**
  * Commands provided by the standard implementation mod.
@@ -28,8 +29,42 @@ public class ModEconomyCommands {
             return 1;
         };
 
-        // Register both literals
+        // Register /bal and /balance
         dispatcher.register(Commands.literal("bal").executes(balanceAction));
         dispatcher.register(Commands.literal("balance").executes(balanceAction));
+
+        // Register the /pay command
+        dispatcher.register(Commands.literal("pay")
+                .then(Commands.argument("target", net.minecraft.commands.arguments.EntityArgument.player())
+                .then(Commands.argument("amount", com.mojang.brigadier.arguments.DoubleArgumentType.doubleArg(0.01))
+                .executes(context -> {
+                    var source = context.getSource();
+                    var sender = source.getPlayerOrException();
+                    var target = net.minecraft.commands.arguments.EntityArgument.getPlayer(context, "target");
+                    var amount = BigDecimal.valueOf(com.mojang.brigadier.arguments.DoubleArgumentType.getDouble(context, "amount"));
+
+                    if (sender.getUUID().equals(target.getUUID())) {
+                        source.sendFailure(Component.literal("You cannot pay yourself!").withStyle(ChatFormatting.RED));
+                        return 0;
+                    }
+
+                    var economy = EconomyManager.getInstance();
+                    var senderBalance = economy.getBalance(sender.getUUID());
+
+                    if (senderBalance.compareTo(amount) < 0) {
+                        source.sendFailure(Component.literal("Insufficient funds!").withStyle(ChatFormatting.RED));
+                        return 0;
+                    }
+
+                    // Perform transaction
+                    economy.removeBalance(sender.getUUID(), amount);
+                    economy.addBalance(target.getUUID(), amount);
+
+                    // Notify
+                    source.sendSuccess(() -> savage.openeconomy.util.EconomyMessages.paySent(target.getGameProfile().name(), amount), false);
+                    target.sendSystemMessage(savage.openeconomy.util.EconomyMessages.payReceived(sender.getGameProfile().name(), amount));
+
+                    return 1;
+                }))));
     }
 }
